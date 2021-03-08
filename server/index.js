89cc384bb18f9bce48c98967e13a0a6d9a87f99d
Gabriellji <gabriellji@yahoo.com>
2021-03-09 00:44:10 +0300

    feat: render to node stream

--- server/index.js
+++ server/index.js
@@ -1,28 +1,38 @@
-import App from '../src/App';
-import React from 'react';
-import { ServerLocation } from '@reach/router';
-import express from 'express';
-import fs from 'fs';
-import { renderToString } from 'react-dom/server';
+import App from "../src/App";
+import React from "react";
+import { ServerLocation } from "@reach/router";
+import express from "express";
+import fs from "fs";
+import { renderToNodeStream } from "react-dom/server";
 
 const PORT = process.env.PORT || 3000;
 
-const html = fs.readFileSync('dist/index.html').toString();
+const html = fs.readFileSync("dist/index.html").toString();
 
-const parts = html.split('not rendered');
+const parts = html.split("not rendered");
 
 const app = express();
 
-app.use('/dist', express.static("dist"));
+app.use("/dist", express.static("dist"));
 app.use((req, res) => {
-    const reactMarkup = (
-        <ServerLocation url={req.url}>
-            <App/>
-        </ServerLocation>
-    )
-    res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
-    res.end();
-})
+  res.write(parts[0]);
+  const reactMarkup = (
+    <ServerLocation url={req.url}>
+      <App />
+    </ServerLocation>
+  );
+  const stream = renderToNodeStream(reactMarkup);
 
-console.log('listening on ' + PORT);
+  stream.pipe(
+    res,
+    { end: false }
+  );
+
+  stream.on("end", () => {
+    res.write(parts[1]);
+    res.end();
+  });
+});
+
+console.log("listening on " + PORT);
 app.listen(PORT);
