310c0c0b7fb68574830477b46b687aef8d72f8ca
Gabriellji <gabriellji@yahoo.com>
2021-03-09 00:35:39 +0300

    feat: server side rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { Link, Router } from "@reach/router";
-import React, { Suspense, lazy, useState } from "react";
+import React, { useState } from "react";
 
-import ReactDOM from "react-dom";
+import  Details from './Details'
+import  SearchParams from './SearchParams'
 import ThemeContext from "./ThemeContext";
-
-const Details = lazy(() => import('./Details'))
-const SearchParams = lazy(() => import('./SearchParams'))
 
 const App = () => {
   const theme = useState("darkblue");
@@ -15,15 +13,13 @@ const App = () => {
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
-        <Suspense fallback={<h1>Loading...</h1>}>
         <Router>
           <SearchParams path="/" />
           <Details path="/details/:id" />
         </Router>
-        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
